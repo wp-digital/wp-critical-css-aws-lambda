6e46d28e726d254faa36c995d089c78c36fc78f8
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2018-01-04 12:15:18 +0200

    add secret auth

--- class-critical-css-aws-lambda.php
+++ class-critical-css-aws-lambda.php
@@ -8,6 +8,8 @@ use Aws\Lambda\LambdaClient;
 class WP_Critical_CSS_AWS_Lambda
 {
     const KEY = 'aws_lambda_critical_css';
+    const API_VERSION = '1';
+    const SECRET_LENGTH = 20;
     /**
      * @var LambdaClient
      */
@@ -16,6 +18,10 @@ class WP_Critical_CSS_AWS_Lambda
      * @var string
      */
     protected $_lambda_function = '';
+    /**
+     * @var string
+     */
+    protected $_site_key = '';
     /**
      * @var string
      */
@@ -29,25 +35,55 @@ class WP_Critical_CSS_AWS_Lambda
      */
     protected $_styles = [];
     /**
+     * @var array
+     */
+    protected $_registered_styles = [];
+    /**
      * @var string
      */
     protected $_url = '';
     /**
      * @var string
      */
-    protected $_version = '';
+    protected $_hash = '';
 
     public function __construct()
     {
-        if ( static::has_required_constants() ) {
-            $this->_template_name = static::get_template_name();
-            $this->_key = static::KEY . '_' . sanitize_key( $this->_template_name );
-            $this->_styles = static::get_styles();
-            $this->_version = $this->_get_version();
+        $this->_styles = static::get_styles();
 
-            if ( false === ( $version = get_transient( $this->_key ) ) || $version != $this->_version ) {
-                $this->_lambda_client = static::get_lambda_client();
-                $this->_lambda_function = static::get_lambda_function();
+        if ( !empty( $this->_styles ) ) {
+            $this->_site_key = static::get_site_key();
+            $secret_key = static::key( 'secret' );
+
+            add_action( $secret_key, function () {
+                if ( static::has_required_constants() ) {
+                    $this->_init_lambda();
+                    $this->_generate_credentials();
+                }
+            } );
+
+            if ( false === get_transient( $secret_key ) ) {
+                wp_schedule_single_event( time(), $secret_key );
+            }
+
+            add_action( static::KEY, function ( $key, $hash, $args ) {
+                if ( static::has_required_constants() ) {
+                    $this->_init_lambda();
+                    $this->_run( $key, $hash, $args );
+                }
+            }, 10, 3 );
+        }
+    }
+
+    public function init()
+    {
+        if ( !empty( $this->_styles ) && static::has_required_constants() ) {
+            $this->_registered_styles = $this->_get_registered_styles();
+
+            if ( !empty( $this->_registered_styles ) ) {
+                $this->_template_name = static::get_template_name();
+                $this->_key = static::key( $this->_template_name );
+                $this->_hash = $this->_get_hash();
                 $this->_url = static::get_current_url();
             }
         }
@@ -60,46 +96,163 @@ class WP_Critical_CSS_AWS_Lambda
      */
     public function run()
     {
-        if ( !is_null( $this->_lambda_client ) ) {
-            $result = $this->_lambda_client->invoke( [
-                'FunctionName' => $this->_lambda_function,
-                'Payload'      => json_encode( $this->_get_lambda_args() ),
+        if (
+            !empty( $this->_key ) && !empty( $this->_hash )
+            && ( false === ( $hash = get_transient( $this->_key ) ) || $hash != $this->_hash )
+            && get_option( $this->_key ) !== $this->_hash
+        ) {
+            wp_schedule_single_event( time() + 10, static::KEY, [
+                $this->_key,
+                $this->_hash,
+                $this->_get_lambda_args(),
             ] );
 
-            if ( $result['StatusCode'] < WP_Http::OK && $result['StatusCode'] >= WP_Http::MULTIPLE_CHOICES ) {
-                return new WP_Error( static::KEY, $result['FunctionError'] );
-            }
-
-            set_transient( $this->_key, $this->_version );
-
-            return $this->_version;
+            return $this->_hash;
         }
 
         return false;
     }
 
-    /**
-     * Cron task
-     */
-    public function schedule_receiving()
+    public function print_stylesheet()
     {
-        wp_schedule_single_event( time() + 15 * MINUTE_IN_SECONDS, static::KEY, [
-            $this->_key,
-            $this->_version,
+        $stylesheet = get_option( "{$this->_key}_stylesheet" );
 
-        ] );
+        if ( !empty( $stylesheet ) ) : ?>
+            <style>
+                <?= strip_tags( $stylesheet ) ?>
+            </style>
+        <?php endif;
+    }
+
+    public function register_rest_route()
+    {
+        if ( !empty( $this->_styles ) && static::has_required_constants() ) {
+            register_rest_route( static::get_api_namespace(), '/stylesheet', [
+                [
+                    'methods'             => WP_REST_Server::CREATABLE,
+                    'callback'            => function ( WP_REST_Request $request ) {
+                        return $this->_create_stylesheet( $request );
+                    },
+                    'permission_callback' => function ( WP_REST_Request $request ) {
+                        return $this->_create_stylesheet_permissions_check( $request );
+                    },
+                    'args'                => [
+                        'key'        => [
+                            'required'          => true,
+                            'sanitize_callback' => function ( $key ) {
+                                return sanitize_key( $key );
+                            },
+                            'validate_callback' => function ( $key ) {
+                                return false !== get_transient( $key );
+                            },
+                        ],
+                        'hash'       => [
+                            'required'          => true,
+                            'validate_callback' => function ( $hash ) {
+                                return is_string( $hash ) && static::is_md5( $hash );
+                            },
+                        ],
+                        'stylesheet' => [
+                            'required'          => true,
+                            'sanitize_callback' => function ( $stylesheet ) {
+                                return strip_tags( $stylesheet );
+                            },
+                        ],
+                        'secret'     => [
+                            'required'          => true,
+                            'validate_callback' => function ( $secret ) {
+                                return is_string( $secret );
+                            },
+                        ],
+                    ],
+                ],
+            ] );
+        }
     }
 
     /**
-     * Return critical path version
+     * @param WP_REST_Request $request
+     *
+     * @return bool|WP_Error
+     */
+    protected function _create_stylesheet_permissions_check( WP_REST_Request $request )
+    {
+        if ( false === ( $secret_hash = get_transient( static::key( 'secret' ) ) ) || !wp_check_password( $request->get_param( 'secret' ), $secret_hash ) ) {
+            return new WP_Error( 'rest_cannot_create_stylesheet', __( 'Sorry, you are not allowed to create critical stylesheet.' ), [
+                'status' => rest_authorization_required_code(),
+            ] );
+        }
+
+        return true;
+    }
+
+    /**
+     * @param WP_REST_Request $request
+     *
+     * @return bool|WP_Error
+     */
+    protected function _create_stylesheet( WP_REST_Request $request )
+    {
+        $key = $request->get_param( 'key' );
+        $hash = $request->get_param( 'hash' );
+
+        if ( $hash !== get_transient( $key ) ) {
+            return new WP_Error( 'rest_invalid_param', __( 'Invalid key or hash.' ), [
+                'status' => WP_Http::BAD_REQUEST,
+            ] );
+        }
+
+        update_option( $key, $hash );
+        delete_transient( $key );
+
+        return update_option( "{$key}_stylesheet", $request->get_param( 'stylesheet' ), false );
+    }
+
+    protected function _init_lambda()
+    {
+        $this->_lambda_client = static::get_lambda_client();
+        $this->_lambda_function = static::get_lambda_function();
+    }
+
+    /**
+     * Return critical path hash
      *
      * @return string
      */
-    protected function _get_version()
+    protected function _get_hash()
     {
-        return array_reduce( $this->_styles, function ( $version, array $style ) {
+        return md5( array_reduce( $this->_registered_styles, function ( $version, array $style ) {
             return "$version|{$style['handle']}:{$style['ver']}";
-        }, '' );
+        }, '' ) );
+    }
+
+    /**
+     * Return URL's of stylesheets which are used for critical path
+     *
+     * @return array
+     */
+    protected function _get_registered_styles()
+    {
+        global $wp_styles, $wp_version;
+
+        $styles = [];
+
+        if ( isset( $wp_styles ) ) {
+            $registered_styles = $wp_styles->registered;
+
+            foreach ( $this->_styles as $handle ) {
+                if ( isset( $registered_styles[ $handle ] ) ) {
+                    $style = $registered_styles[ $handle ];
+                    $styles[] = [
+                        'handle' => $style->handle,
+                        'src'    => $style->src,
+                        'ver'    => $style->ver ? $style->ver : apply_filters( static::key( 'default_style_version' ), $wp_version ),
+                    ];
+                }
+            }
+        }
+
+        return $styles;
     }
 
     /**
@@ -110,12 +263,58 @@ class WP_Critical_CSS_AWS_Lambda
     protected function _get_lambda_args()
     {
         return [
-            'bucket'        => AWS_LAMBDA_CRITICAL_CSS_BUCKET,
-            'template_name' => $this->_template_name,
-            'styles'        => array_column( $this->_styles, 'src' ),
-            'url'           => $this->_url,
-            'version'       => $this->_version,
+            'key'        => $this->_key,
+            'styles'     => array_column( $this->_registered_styles, 'src' ),
+            'url'        => $this->_url,
+            'hash'       => $this->_hash,
+            'return_url' => static::get_rest_url( '/stylesheet' ),
+            'site_key'   => $this->_site_key,
         ];
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _generate_credentials()
+    {
+        $result = $this->_lambda_client->getFunctionConfiguration( [
+            'FunctionName' => $this->_lambda_function,
+        ] );
+        $environment = $result->get( 'Environment' );
+        $vars = !is_null( $environment ) && isset( $environment['Variables'] ) && is_array( $environment['Variables'] ) ? $environment['Variables'] : [];
+        $secret = wp_generate_password( static::SECRET_LENGTH );
+        $vars[ $this->_site_key ] = $secret;
+        $this->_lambda_client->updateFunctionConfiguration( [
+            'FunctionName' => $this->_lambda_function,
+            'Environment'  => [
+                'Variables' => $vars,
+            ],
+        ] );
+
+        return set_transient( static::key( 'secret' ), wp_hash_password( $secret ), WEEK_IN_SECONDS );
+    }
+
+    /**
+     *
+     *
+     * @param string $key
+     * @param string $hash
+     * @param array  $args
+     *
+     * @return bool|WP_Error
+     */
+    protected function _run( $key, $hash, $args )
+    {
+        $result = $this->_lambda_client->invoke( [
+            'FunctionName' => $this->_lambda_function,
+            'Payload'      => json_encode( $args ),
+        ] );
+
+        if ( $result[ 'StatusCode' ] < WP_Http::OK || $result[ 'StatusCode' ] >= WP_Http::MULTIPLE_CHOICES ) {
+            return new WP_Error( static::KEY, $result[ 'FunctionError' ] );
+        }
+
+        return set_transient( $key, $hash, DAY_IN_SECONDS / 2 );
     }
 
     /**
@@ -125,7 +324,7 @@ class WP_Critical_CSS_AWS_Lambda
      */
     public static function has_required_constants()
     {
-        return defined( 'AWS_LAMBDA_CRITICAL_CSS_KEY' ) && defined( 'AWS_LAMBDA_CRITICAL_CSS_SECRET' ) && defined( 'AWS_LAMBDA_CRITICAL_CSS_REGION' ) && defined( 'AWS_LAMBDA_CRITICAL_CSS_BUCKET' );
+        return defined( 'AWS_LAMBDA_CRITICAL_CSS_KEY' ) && defined( 'AWS_LAMBDA_CRITICAL_CSS_SECRET' ) && defined( 'AWS_LAMBDA_CRITICAL_CSS_REGION' );
     }
 
     /**
@@ -152,7 +351,15 @@ class WP_Critical_CSS_AWS_Lambda
      */
     public static function get_lambda_function()
     {
-        return defined( 'AWS_LAMBDA_CRITICAL_CSS_FUNCTION' ) ? AWS_LAMBDA_CRITICAL_CSS_FUNCTION : 'wordpress_critical_css';
+        return defined( 'AWS_LAMBDA_CRITICAL_CSS_FUNCTION' ) ? AWS_LAMBDA_CRITICAL_CSS_FUNCTION : 'wordpress_critical_css-production';
+    }
+
+    /**
+     * @return string
+     */
+    public static function get_site_key()
+    {
+        return static::sanitize_site_key( get_site_url() );
     }
 
     /**
@@ -168,29 +375,11 @@ class WP_Critical_CSS_AWS_Lambda
     }
 
     /**
-     * Return URL's of stylesheets which are used for critical path
-     *
      * @return array
      */
     public static function get_styles()
     {
-        global $wp_styles, $wp_version;
-
-        $registered_styles = $wp_styles->registered;
-        $styles = [];
-
-        foreach ( apply_filters( static::KEY . '_styles', [] ) as $handle ) {
-            if ( isset( $registered_styles[ $handle ] ) ) {
-                $style = $registered_styles[ $handle ];
-                $styles[] = [
-                    'handle' => $style->handle,
-                    'src'    => $style->src,
-                    'ver'    => $style->ver ? $style->ver : $wp_version,
-                ];
-            }
-        }
-
-        return $styles;
+        return apply_filters( static::key( 'styles' ), [] );
     }
 
     /**
@@ -201,5 +390,53 @@ class WP_Critical_CSS_AWS_Lambda
     public static function get_current_url()
     {
         return home_url( $_SERVER['REQUEST_URI'] );
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return string
+     */
+    public static function key( $key )
+    {
+        return static::KEY . '_' . sanitize_key( $key );
+    }
+
+    /**
+     * @return string
+     */
+    public static function get_api_namespace()
+    {
+        return static::KEY . '/v' . static::API_VERSION;
+    }
+
+    /**
+     * @param string $endpoint
+     *
+     * @return string
+     */
+    public static function get_rest_url( $endpoint )
+    {
+        return esc_url_raw( rest_url( static::get_api_namespace() . $endpoint ) );
+    }
+
+    /**
+     * @param string $hash
+     *
+     * @return int
+     */
+    public static function is_md5( $hash )
+    {
+        return preg_match( '/^[a-f0-9]{32}$/', $hash );
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return string
+     */
+    public static function sanitize_site_key( $key )
+    {
+        return preg_replace( '/[^a-zA-Z0-9_]/', '', $key );
     }
 }
--- wp-critical-css-aws-lambda.php
+++ wp-critical-css-aws-lambda.php
@@ -2,32 +2,62 @@
 /**
  * Plugin Name: AWS Lambda Critical CSS
  * Description: Generates critical stylesheets for templates via AWS Lambda
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Innocode
  * Author URI: https://innocode.com
  * Requires at least: 4.8
- * Tested up to: 4.8.1
+ * Tested up to: 4.9.2
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-define( 'AWS_LAMBDA_CRITICAL_CSS_VERSION', '0.1.0' );
+define( 'AWS_LAMBDA_CRITICAL_CSS_VERSION', '0.1.1' );
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/class-critical-css-aws-lambda.php';
 
-function aws_lambda_critical_css_run() {
-    $aws_lambda_critical_css = new WP_Critical_CSS_AWS_Lambda();
+function aws_lambda_critical_css() {
+    $GLOBALS['aws_lambda_critical_css'] = new WP_Critical_CSS_AWS_Lambda();
+}
 
-    if ( false !== ( $version = $aws_lambda_critical_css->run() ) && !is_wp_error( $version ) ) {
-        $aws_lambda_critical_css->schedule_receiving();
+add_action( 'init', 'aws_lambda_critical_css', 99 );
+
+function aws_lambda_critical_css_init() {
+    /**
+     * @var WP_Critical_CSS_AWS_Lambda $aws_lambda_critical_css
+     */
+    global $aws_lambda_critical_css;
+
+    if ( isset( $aws_lambda_critical_css ) ) {
+        $aws_lambda_critical_css->init();
     }
 }
 
-add_action( 'wp_head', 'aws_lambda_critical_css_run', 1 );
+add_action( 'wp_enqueue_scripts', 'aws_lambda_critical_css_init', 99 );
 
-add_action( WP_Critical_CSS_AWS_Lambda::KEY, function ( $key, $version ) {
-    if ( false !== ( $_version = get_transient( $key ) ) && $_version == $version ) {
+function aws_lambda_critical_css_rest_api_init() {
+    /**
+     * @var WP_Critical_CSS_AWS_Lambda $aws_lambda_critical_css
+     */
+    global $aws_lambda_critical_css;
 
+    if ( isset( $aws_lambda_critical_css ) ) {
+        $aws_lambda_critical_css->register_rest_route();
     }
-}, 10, 2 );
+}
+
+add_action( 'rest_api_init', 'aws_lambda_critical_css_rest_api_init' );
+
+function aws_lambda_critical_css_run() {
+    /**
+     * @var WP_Critical_CSS_AWS_Lambda $aws_lambda_critical_css
+     */
+    global $aws_lambda_critical_css;
+
+    if ( isset( $aws_lambda_critical_css ) ) {
+        $aws_lambda_critical_css->run();
+        $aws_lambda_critical_css->print_stylesheet();
+    }
+}
+
+add_action( 'wp_head', 'aws_lambda_critical_css_run', 2 );
